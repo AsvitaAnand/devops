2f00ad78c5fec67a620fd699fe082e60251ff1ca
AsvitaAnand <127740114+AsvitaAnand@users.noreply.github.com>
2025-02-05 10:48:04 +0530

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-    echo hi123
-    sh 'chmod +x build.sh'
-    sh './build.sh'
-    docker login -u asvita12 -p asvinu@12
-    docker tag test asvita12/task2
-    docker push asvita12/task2
-    
